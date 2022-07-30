08f23cbe53a57d93b579703d2f4c36fcd60e38d4
littlema <j620178@gmail.com>
2022-07-30 21:51:07 +0800

    Initial Commit

---
+++ PokemonCarnival/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonCarnival
+//
+//  Created by littlema on 2022/7/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
