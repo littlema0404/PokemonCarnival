af48ff130cc1e4e539b0dd46fa527b171b570914
littlema <j620178@gmail.com>
2022-07-31 14:34:37 +0800

    Add extension UIImageView

---
+++ PokemonCarnival/Extensions/UIImageView+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIImageView+Extension.swift
+//  PokemonCarnival
+//
+//  Created by littlema on 2022/7/31.
+//
+
+import UIKit
+import Kingfisher
+
+extension UIImageView {
+    func setImage(url: URL, placeHolder: UIImage? = nil) {
+        kf.setImage(with: url, placeholder: placeHolder)
+    }
+}
